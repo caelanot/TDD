c06f00e0e78d91d61282a6c44a55e032bc3f6998
Caelan <caelanotoole@gmail.com>
2023-10-03 15:28:32 -0700

    CI Lab counters updated

--- src/counter.py
+++ src/counter.py
@@ -1,6 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
 import status
+app = Flask(__name__)
 
 COUNTERS = {}
 
@@ -8,28 +8,46 @@ COUNTERS = {}
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
+
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-      return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return ({"Message": f"Counter {name} already exists"},
+                status.HTTP_409_CONFLICT)
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
+
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
-  """Update a counter"""
-  app.logger.info(f"Incrementing value at counter: {name}")
-  global COUNTERS
-  if name in COUNTERS:
-    COUNTERS[name] += 1
-    return {name: COUNTERS[name]}, status.HTTP_200_OK
+    """Update a counter"""
+    app.logger.info(f"Incrementing value at counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        COUNTERS[name] += 1
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
+
 
 @app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
-  """Get a counter"""
-  app.logger.info(f"Getting counter {name}")
-  if name in COUNTERS:
-    return {name: COUNTERS[name]}, status.HTTP_200_OK
+    """Get a counter"""
+    app.logger.info(f"Getting counter {name}")
+    if name in COUNTERS:
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
+    else:
+        return {name: None}, status.HTTP_404_NOT_FOUND
+
+
+@app.route('/counters/<name>', methods=["DELETE"])
+def delete_counter(name):
+    """Delete a counter"""
+    app.logger.info(f"Deleting counter {name}")
+    if name in COUNTERS:
+        COUNTERS.pop(name)
+        return {name: None}, status.HTTP_204_NO_CONTENT
+    else:
+        return {name: None}, status.HTTP_404_NOT_FOUND
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,8 +1,17 @@
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
 """
 Test Cases for Counter Web Service
 
 Create a service that can keep a track of multiple counters
-- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
+- API must be RESTful - see the status.py file. Following these guidelines,
+you can make assumptions about
 how to call the web service and assert what it should return.
 - The endpoint should be called /counters
 - When creating a counter, you must specify the name in the path.
@@ -11,47 +20,59 @@ how to call the web service and assert what it should return.
 - The service must be able to read the counter
 """
 
-from unittest import TestCase
-
-# we need to import the unit under test - counter
-from src.counter import app 
-
-# we need to import the file that contains the status codes
-from src import status 
 
 class CounterTest(TestCase):
     """Counter tests"""
-    
+
     def setUp(self):
-      self.client = app.test_client()
-    
+        self.client = app.test_client()
+
     def test_create_a_counter(self):
-     """It should create a counter"""
-     result = self.client.post('/counters/foo')
-     self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-    
+        """It should create a counter"""
+        result = self.client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
     def test_duplicate_a_counter(self):
-      """It should return an error for duplicates"""
-      result = self.client.post('/counters/bar')
-      self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-      result = self.client.post('/counters/bar')
-      self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
-    
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
     def test_update_a_counter(self):
-      """Should update a counter by 1"""
-      result = self.client.post('/counters/baz')
-      self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-      baseline = result.get_json()["baz"]
-      
-      result = self.client.put('/counters/baz')
-      self.assertEqual(result.status_code, status.HTTP_200_OK)
-      
-      self.assertEqual(baseline + 1, result.get_json()["baz"])
-    
+        """Should update a counter by 1"""
+        result = self.client.post('/counters/baz')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        baseline = result.get_json()["baz"]
+
+        result = self.client.put('/counters/baz')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(baseline + 1, result.get_json()["baz"])
+
     def test_get_counter(self):
-      """Should read a counter value"""
-      result = self.client.post('/counters/boz')
-      self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-      
-      result = self.client.get('/counters/boz')
-      self.assertEqual(result.status_code, status.HTTP_200_OK)
+        """Should read a counter value"""
+        result = self.client.post('/counters/boz')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+        result = self.client.get('/counters/boz')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+    
+    def test_delete_counter(self):
+        """Should delete a counter"""
+        
+        # Create a counter
+        result = self.client.post('/counters/fizz')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        
+        # Deleting an existing counter should 204
+        result = self.client.delete('/counters/fizz')
+        self.assertEqual(result.status_code, status.HTTP_204_NO_CONTENT)
+        
+        # Deleted counter should not return value
+        result = self.client.get('/counters/fizz')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+        
+        # Deleting a deleted/non-existent counter should 404
+        result = self.client.delete('/counters/fizz')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
